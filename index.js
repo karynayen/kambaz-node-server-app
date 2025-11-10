41ba5524b4bf9000d50f4fa9ec4b5ea612fad1b9
Karyna Yen <kyen@hubspot.com>
2025-11-09 21:30:12 -0500

    up to 5.3.3 Supporting Multiple User Sessions

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import express from 'express'
 import Hello from "./Hello.js"
 import cors from 'cors'
 import Lab5 from "./Lab5/index.js";
-const app = express()
+import db from "./Kambaz/Database/index.js";
+import UserRoutes from "./Kambaz/Users/routes.js";
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+UserRoutes(app, db);
 
 Lab5(app);
 Hello(app)
